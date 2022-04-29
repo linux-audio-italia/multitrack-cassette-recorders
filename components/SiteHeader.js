b3261fd7bd6413d24a4ad4de08c38dae19966833
Ascani Carlo <carlotm@pm.me>
2022-04-29 15:15:06 +0200

    Add a link to all models page

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -5,6 +5,7 @@ const SiteHeader = () => (
     <nav role="navigation" className="limit">
       <Link href="/">Multitrack Cassette Recorders</Link>
       <Link href="/recorders">Brands</Link>
+      <Link href="/models">Models</Link>
       <Link href="/about">About</Link>
     </nav>
   </header>
