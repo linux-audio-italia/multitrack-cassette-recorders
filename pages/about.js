fa024b5114f5e294969db3f9055efe8566126455
Emiliano Grilli <emiliano.grilli@gmail.com>
2022-05-01 11:35:58 +0200

    Added citation in about page

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,21 @@ const About = () => (
       <h1>About</h1>
     </header>
     <div className="Page-content">
-      <p>TODO</p>
+      <figure>
+        <blockquote>
+          In the list of the most brilliant inventions the human race has ever
+          devised, third only to the bicycle and the grand piano is the
+          cassette-based multitrack recorder, otherwise commonly known as the
+          Portastudio.
+        </blockquote>
+        <figcaption>
+          David Mellor, <cite>&quot;Hands On: Cassette Multitracks&quot;</cite>{" "}
+          on{" "}
+          <a href="http://www.muzines.co.uk/articles/hands-on-cassette-multitracks/9522">
+            Sound On Sound - Oct 1992
+          </a>
+        </figcaption>
+      </figure>
     </div>
   </article>
 );
