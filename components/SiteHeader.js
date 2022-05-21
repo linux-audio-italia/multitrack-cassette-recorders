4b47804087b07f3014ccbb5ce5d695777067f0a0
Ascani Carlo <carlotm@pm.me>
2022-05-21 10:20:05 +0200

    Try with a pattern in the header

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 const SiteHeader = () => (
   <header className="SiteHeader">
+    <Link href="/">
+      <a className="SiteHeader-logo">Multitrack Cassette</a>
+    </Link>
     <nav role="navigation" className="limit">
-      <Link href="/">Multitrack Cassette Recorders</Link>
-      <Link href="/brands">All brands</Link>
-      <Link href="/models">All models</Link>
+      <Link href="/brands">Brands</Link>
+      <Link href="/models">Models</Link>
       <Link href="/about">About</Link>
     </nav>
   </header>
