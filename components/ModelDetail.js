cfa488e9830e41e5eb16e7f40791efeab15049fb
Emiliano Grilli <emiliano.grilli@gmail.com>
2022-05-24 13:44:43 +0200

    more fields in ModelDetail
    - Conditional rendering of fields
    - added fostex x28h and fostex x26 data

--- components/ModelDetail.js
+++ components/ModelDetail.js
@@ -1,5 +1,12 @@
 import { safe } from "../lib/utils";
 
+const DefinitionItem = (title, value) => (
+  <>
+    <dt>{title}</dt>
+    <dd>{safe(value)}</dd>
+  </>
+);
+
 const ModelDetail = ({ recorder }) => (
   <article className="Page">
     <header className="Page-head">
@@ -9,12 +16,18 @@ const ModelDetail = ({ recorder }) => (
     </header>
     <div className="Page-content ModelDetail">
       <dl className="ModelDetail-features">
-        <dt>Year</dt>
-        <dd>{safe(recorder.year)}</dd>
-        <dt>Mixer channels</dt>
-        <dd>{safe(recorder.mix_channels)}</dd>
-        <dt>Recording channels</dt>
-        <dd>{safe(recorder.mix_channels)}</dd>
+        {recorder.year && DefinitionItem("Year", recorder.year)}
+        {recorder.mix_channels &&
+          DefinitionItem("Mixer channels", recorder.mix_channels)}
+        {recorder.rec_tracks &&
+          DefinitionItem("Recording tracks", recorder.rec_tracks)}
+        {recorder.max_simultaneous_rec_tracks &&
+          DefinitionItem(
+            "Max tracks recordable at once",
+            recorder.max_simultaneous_rec_tracks
+          )}
+        {recorder.low_speed && DefinitionItem("Low speed", recorder.low_speed)}
+        {recorder.hi_speed && DefinitionItem("High speed", recorder.hi_speed)}
       </dl>
     </div>
   </article>
