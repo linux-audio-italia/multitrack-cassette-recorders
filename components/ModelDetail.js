892d8c1bdc38dee69c9d28ea0868ee5a283a713e
Emiliano Grilli <emiliano.grilli@gmail.com>
2022-05-24 14:29:41 +0200

    More fields in model detail, updated fostex x-28h

--- components/ModelDetail.js
+++ components/ModelDetail.js
@@ -19,6 +19,11 @@ const ModelDetail = ({ recorder }) => (
         {recorder.year && DefinitionItem("Year", recorder.year)}
         {recorder.mix_channels &&
           DefinitionItem("Mixer channels", recorder.mix_channels)}
+        {recorder.chan_eq && DefinitionItem("Channel eq", recorder.chan_eq)}
+        {recorder.master_eq && DefinitionItem("Master eq", recorder.master_eq)}
+        {recorder.aux_sends && DefinitionItem("Aux sends", recorder.aux_sends)}
+        {recorder.aux_returns &&
+          DefinitionItem("Aux returns", recorder.aux_returns)}
         {recorder.rec_tracks &&
           DefinitionItem("Recording tracks", recorder.rec_tracks)}
         {recorder.max_simultaneous_rec_tracks &&
@@ -26,8 +31,15 @@ const ModelDetail = ({ recorder }) => (
             "Max tracks recordable at once",
             recorder.max_simultaneous_rec_tracks
           )}
+        {recorder.individual_track_outs &&
+          DefinitionItem(
+            "Individual track outs",
+            recorder.individual_track_outs
+          )}
         {recorder.low_speed && DefinitionItem("Low speed", recorder.low_speed)}
         {recorder.hi_speed && DefinitionItem("High speed", recorder.hi_speed)}
+        {recorder.noise_reduction &&
+          DefinitionItem("Noise reduction", recorder.noise_reduction)}
       </dl>
     </div>
   </article>
