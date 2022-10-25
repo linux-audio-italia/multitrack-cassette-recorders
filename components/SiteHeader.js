ba667eb43f1f7fe7927d8a20952a1f68454a6313
Ascani Carlo <carlotm@pm.me>
2022-10-25 08:15:31 +0200

    Fix padding around h1 on mobile

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 const SiteHeader = () => (
   <header className="SiteHeader">
-    <Link href="/">
-      <a className="SiteHeader-logo">Multitrack Cassette</a>
-    </Link>
-    <nav role="navigation" className="limit">
-      <Link href="/brands">Brands</Link>
-      <Link href="/models">Models</Link>
-      <Link href="/about">About</Link>
-    </nav>
+    <div className="limit">
+      <Link href="/">
+        <a className="SiteHeader-logo">Multitrack Cassette</a>
+      </Link>
+      <nav role="navigation" className="limit">
+        <Link href="/brands">Brands</Link>
+        <Link href="/models">Models</Link>
+        <Link href="/about">About</Link>
+      </nav>
+    </div>
   </header>
 );
 
